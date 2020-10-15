e3daaedfbefd2973dcfb3ccca724371755f00fba
Wondwosen Tsige <wondneh@gmail.com>
2020-10-15 13:14:20 -0700

    write some fuction

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var userName = prompt('Please Enter your name!');
+/*var userName = prompt('Please Enter your Name');
 
-alert('Welcome ' + userName + 'Click ok to continue');
+alert('Welcome ' + userName + 'Click ok to continue');*/
 
+function getUserName (){
+    var userName = prompt('Please Enter your Name');
+    return userName;
+}
+
+function greetUser (userName){
+    alert('Welcome ' + userName + 'Click ok to continue');
+}
+
+var userName=getUserName();
+greetUser(userName);
