adaf20ae1ff745c20049addbd20fe83dd3dbfc32
Wondwosen Tsige <wondneh@gmail.com>
2020-10-14 20:39:35 -0700

    tried to make my page active

---
+++ app.js
@@ -0,0 +1,4 @@
+var userName = prompt('Please Enter your name!');
+
+alert('Welcome ' + userName + 'Click ok to continue');
+
---
+++ placestovisit.js
@@ -0,0 +1,13 @@
+var toVisitFirst = prompt('Where do you want to go first? ');
+var toVisitSecond = prompt('Where do you want to go second? ');
+var toVisitThird = prompt('Where do you want to go last? ');
+
+document.write('I want to visit')
+document.write(
+   
+   '<li>' + '1st ' + toVisitFirst +  '</li>' +
+    '<li>' +  '2nd ' + toVisitSecond + '</li>' +
+    '<li>' + 'last ' + toVisitFirst +  '</li>'
+
+
+)
